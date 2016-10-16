9a55d6dc298bb091d6d0ca55ef152145ad650457
Paulo Clavijo Esteban <paucls@gmail.com>
2016-10-16 12:02:09 +0100

    Removing not longer used title field from app component. Updated unit tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 describe('App: Ng2PersonalDashboard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
       ],
     });
   });
@@ -18,16 +20,16 @@ describe('App: Ng2PersonalDashboard', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  // it(`should have as title 'app works!'`, async(() => {
+  //   let fixture = TestBed.createComponent(AppComponent);
+  //   let app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('app works!');
+  // }));
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  // it('should render title in a h1 tag', async(() => {
+  //   let fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   let compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  // }));
 });
